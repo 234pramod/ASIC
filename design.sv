f7a3bfed193bb1e0af592b623d0fcb4a472c41b9
234pramod <234pramod@gmail.com>
2019-05-04 13:04:16 +0530

    Update design.sv

--- design.sv
+++ design.sv
@@ -4,7 +4,7 @@ module router(din,
              valid_n,
              reset_n,
              clock,
-             dour,
+             dout,
              frameo_n,
              valido_n);
 
@@ -16,4 +16,13 @@ module router(din,
   output dout [15:0];
   output frameo_n[15:0];
   output valido_n[15:0];
+  
+  always @(posedge reset) begin
+    foreach (valid_n[i])begin
+      dout[i] <=din[i];
+      frame_n[i] <= frame_n[i];
+      valido_n  <=  1 ;
+    end
+  end
+  
 endmodule
